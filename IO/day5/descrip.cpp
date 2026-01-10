a01eaa9d7f1a1984cd657045cc62c0976fadb379
TongXinGood <tongxinlovecr@outlook.com>
2026-01-10 20:26:07 +0800

    测试一下演示一下

--- IO/day5/descrip.cpp
+++ IO/day5/descrip.cpp
@@ -2,6 +2,7 @@
 
 int main(){
     //分别输出标准输入、输出、出错文件指针对应的文件描述符
+    //test
     printf("stdin->_fileno = %d\n",stdin->_fileno); 
     printf("stdout->_fileno = %d\n",stdout->_fileno); 
     printf("stderr->_fileno = %d\n",stderr->_fileno); 
