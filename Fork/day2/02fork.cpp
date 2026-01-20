751ed9a8ea8cf164232e3949e5bccfee5ac5b652
TongXinGood <tongxinlovecr@outlook.com>
2026-01-20 15:52:36 +0800

    父进程与子进程

---
+++ Fork/day2/02fork.cpp
@@ -0,0 +1,13 @@
+#include <myhead.h>
+
+int main(){
+    
+    fork();  //创建一个子进程
+    fork();  //创建一个子进程
+    fork();  //创建一个子进程
+    //产生2^n个进程
+
+    while(1);
+
+    return 0;
+}
---
+++ Fork/day2/fork.cpp
@@ -0,0 +1,11 @@
+#include <myhead.h>
+
+int main(){
+    fork();  //创建一个子进程
+
+    printf("hello world\n");
+
+    while(1);
+
+    return 0;
+}
